4e39604fc4f3c62b51e506d388eb8f39cf5d2521
Griffin <gmd298@gmail.com>
2022-03-16 21:28:38 -0400

    commit tree fda750e156df7b1d75d46427df85e47832072f7a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+    return Math.abs(pickUp - 42);
+}
+
+function distanceFromHqInFeet(pickUp) {
+    return distanceFromHqInBlocks(pickUp) * 264;
+}
+
+function distanceTravelledInFeet(pickUp, dropOff) {
+    return Math.abs(pickUp - dropOff) * 264;
+}
+
+function calculatesFarePrice(pickUp, dropOff) {
+    const feetTravelled = distanceTravelledInFeet(pickUp, dropOff);
+    if (feetTravelled < 400) {
+        return 0;
+    }
+    if (feetTravelled > 2500) {
+        return 'cannot travel that far';
+    }
+    if (feetTravelled > 2000) {
+        return 25;
+    }
+    return (feetTravelled - 400) * 0.02;
+}
+
